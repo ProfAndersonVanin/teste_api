6cca26b2011bfdfdb9acbae4cadd3a607eeed2ec
Anderson <profandersonvanin@gmail.com>
2022-05-17 18:46:40 -0300

    teste de commit

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json())
 app.post('/pessoas', async(req,res)=>{
     const {nome,salario,aprovado} = req.body
     if(!nome){
-        res.status(422).json({error:'Nome é obrigatório. Não pode estar vazio!'})
+        res.status(422).json({error:'Nome é obrigatório!'})
     }
     const pessoas = {
         nome,
